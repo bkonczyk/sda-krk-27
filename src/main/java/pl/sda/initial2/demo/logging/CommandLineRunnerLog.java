3bc2101eae9b14f1c53df279666ca7cbd11e6a75
Bartosz Kończyk <bartkon@gmail.com>
2021-04-11 10:59:20 +0200

    initial commit

---
+++ src/main/java/pl/sda/initial2/demo/logging/CommandLineRunnerLog.java
@@ -0,0 +1,16 @@
+package pl.sda.initial2.demo.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CommandLineRunnerLog implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        // tutaj chcemy cos zalogowac po inicjalizacji aplikacji
+        this.log.info("Moj pierwszy komponent");
+    }
+}
